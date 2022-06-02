fc41870dd6336086e81b96ae04e0716a6bb7b181
Tino Reyna <tinoreyna1984@gmail.com>
2022-06-01 23:05:21 -0500

    Nuevos cambios

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * A diferencia de JavaScript, el tipado en TypeScript es muy importante.
+ * Es una herramienta que nos permite definir tipos de datos, así como
+ * también es una buena práctica para evitar errores de tipado.
+ */
+const msg = 'Hola mundo';
+console.log(msg);
---
+++ bases/app.ts
@@ -0,0 +1,10 @@
+/**
+ * A diferencia de JavaScript, el tipado en TypeScript es muy importante.
+ * Es una herramienta que nos permite definir tipos de datos, así como
+ * también es una buena práctica para evitar errores de tipado.
+ */
+
+const msg: string = 'Hola mundo';
+
+console.log(msg);
+
